e4444837f42cdb532178bcfbf06b80dfd381da2f
Zlatin Stanimirov <zlatinstanimirov@Zlatins-MacBook-Pro.local>
2023-11-06 17:59:28 +0200

    feat: export tsoogle as a function

--- index.ts
+++ index.ts
@@ -4,10 +4,16 @@ import { evaluateSearchQuery } from './src/evaluator'
 import { parse } from './src/lexer'
 import { prettify } from './src/prettyfier'
 
-const tsFilePath = process.argv[2]
+const tsConfigFilePath = process.argv[2]
 const userSearchQuery = process.argv[3]
 
-const searchQuery = parse(userSearchQuery)
-evaluateSearchQuery(tsFilePath, searchQuery).forEach(func => {
-  console.log(prettify(func))
-})
+export function tsoogle (tsConfigFilePath: string, userSearchQuery: string): string {
+  const searchQuery = parse(userSearchQuery)
+  let output = ''
+  evaluateSearchQuery(tsConfigFilePath, searchQuery).forEach(func => {
+    output += `${prettify(func)}\n`
+  })
+  return output
+}
+
+console.log(tsoogle(tsConfigFilePath, userSearchQuery))
