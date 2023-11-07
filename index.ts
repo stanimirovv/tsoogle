14640cc8b9264251f03cee83ee165e6c872e8104
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2023-11-07 17:29:44 +0200

    feat: add vizualization type for VsCode plugin

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
-import { evaluateSearchQuery } from './src/evaluator'
+import { evaluateSearchQuery, type FunctionDetail } from './src/evaluator'
 import { parse } from './src/lexer'
 import { prettify } from './src/prettyfier'
 
 const tsConfigFilePath = process.argv[2]
 const userSearchQuery = process.argv[3]
 
-export function tsoogle (tsConfigFilePath: string, userSearchQuery: string): string {
+export function tsoogle (tsConfigFilePath: string, userSearchQuery: string): FunctionDetail[] {
+  const searchQuery = parse(userSearchQuery)
+  return evaluateSearchQuery(tsConfigFilePath, searchQuery)
+}
+
+export function tsoogleCmd (tsConfigFilePath: string, userSearchQuery: string): string {
   const searchQuery = parse(userSearchQuery)
   let output = ''
   evaluateSearchQuery(tsConfigFilePath, searchQuery).forEach(func => {
@@ -16,4 +21,6 @@ export function tsoogle (tsConfigFilePath: string, userSearchQuery: string): str
   return output
 }
 
-console.log(tsoogle(tsConfigFilePath, userSearchQuery))
+if (tsConfigFilePath !== undefined && userSearchQuery !== undefined) {
+  console.log(tsoogleCmd(tsConfigFilePath, userSearchQuery))
+}
