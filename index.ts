a6d778ef2423500a2480e87b1f9ab6c035d171e4
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2023-12-01 16:16:47 +0200

    feat: fix calling as script

--- index.ts
+++ index.ts
@@ -8,22 +8,32 @@ import { prettify } from './src/prettyfier'
 const tsConfigFilePath = process.argv[2]
 const userSearchQuery = process.argv[3]
 
-export function tsoogle (tsConfigFilePath: string, userSearchQuery: string, useIndex = true): FunctionDetail[] {
+export async function tsoogle (tsConfigFilePath: string, userSearchQuery: string, useIndex = true): Promise<FunctionDetail[]> {
   const searchQuery = parse(userSearchQuery)
-  const projectFunctions = getMethodsAndFunctions(searchQuery.kind, tsConfigFilePath, useIndex)
+  const projectFunctions = await getMethodsAndFunctions(searchQuery.kind, tsConfigFilePath, useIndex)
   return evaluateSearchQuery(projectFunctions, searchQuery)
 }
 
-export function tsoogleCmd (tsConfigFilePath: string, userSearchQuery: string, useIndex = true): string {
+export async function tsoogleCmd (tsConfigFilePath: string, userSearchQuery: string, useIndex = true): Promise<string> {
   const searchQuery = parse(userSearchQuery)
   let output = ''
-  const projectFunctions = getMethodsAndFunctions(searchQuery.kind, tsConfigFilePath, useIndex)
+  const projectFunctions = await getMethodsAndFunctions(searchQuery.kind, tsConfigFilePath, useIndex)
   evaluateSearchQuery(projectFunctions, searchQuery).forEach(func => {
     output += `${prettify(func)}\n`
   })
   return output
 }
 
-if (tsConfigFilePath !== undefined && userSearchQuery !== undefined) {
-  console.log(tsoogleCmd(tsConfigFilePath, userSearchQuery))
+async function main (): Promise<void> {
+  if (tsConfigFilePath !== undefined && userSearchQuery !== undefined) {
+    try {
+      const response = await tsoogleCmd(tsConfigFilePath, userSearchQuery)
+      console.log(response)
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+main()
